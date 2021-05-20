5d919879fbe13a20ef8d7edb403b2eebb42e91b9
Hans-J. Schmid <knock@myopendoor.de>
2021-05-20 06:38:47 +0200

    cleans up code.

--- example2/counter.c
+++ example2/counter.c
@@ -1,86 +1,45 @@
-/*
- * HOWTO write an External for Pure data
- * (c) 2001-2006 IOhannes m zmölnig zmoelnig[AT]iem.at
- *
- * this is the source-code for the second example in the HOWTO
- * it creates an object that increments and outputs a counter
- * whenever it gets banged.
- *
- * for legal issues please see the file LICENSE.txt
- */
-
-
-
-/**
- * include the interface to Pd
- */
 #include "m_pd.h"
 
-/**
- * define a new "class"
- */
+// define a new "class"
 static t_class *counter_class;
 
-
-
-/**
- * this is the dataspace of our new object
- * the first (mandatory) "t_object"
- * and a variable that holds the current counter value
- */
+// this is the dataspace of our new object; the first (mandatory) "t_object"
 typedef struct _counter {
-  t_object  x_obj;
-  t_int i_count;
+  t_object x_obj;
+  t_float  i_count;
 } t_counter;
 
-
-/**
- * this method is called whenever a "bang" is sent to the object
- * a reference to the class-dataspace is given as argument
- * this enables us to do something with the data (e.g. increment the counter)
- */
-void counter_bang(t_counter *x)
+void counter_bang(t_counter* x)
 {
-  /*
-   * convert the current counter value to floating-point to output it later
-   */
-  t_float f=x->i_count;
-  /* increment the counter */
-  x->i_count++;
-  /* send the old counter-value to the 1st outlet of the object */
-  outlet_float(x->x_obj.ob_outlet, f);
+  outlet_float(x->x_obj.ob_outlet, x->i_count++);
 }
 
-
-/**
- * this is the "constructor" of the class
- * we have one argument of type floating-point (as specified below in the counter_setup() routine)
- */
-void *counter_new(t_floatarg f)
+// "constructor" of the class
+// we have one argument of type floating-point (as specified below in the counter_setup() routine)
+void* counter_new(t_floatarg f)
 {
-  t_counter *x = (t_counter *)pd_new(counter_class);
-
-  /* set the counter value to the given argument */
+  post("Constructor: %f", f);
+  t_counter* x = (t_counter *)pd_new(counter_class);
   x->i_count=f;
-
-  /* create a new outlet for floating-point values */
+  // create a new outlet for floating-point values
   outlet_new(&x->x_obj, &s_float);
 
   return (void *)x;
 }
 
-
-/**
- * define the function-space of the class
- */
+// function-space of the class
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
 void counter_setup(void) {
   counter_class = class_new(gensym("counter"),
-      (t_newmethod)counter_new,
-      0,
-      sizeof(t_counter),
-      CLASS_DEFAULT,
-      A_DEFFLOAT, 0); /* the object takes one argument which is a floating-point and defaults to 0 */
+                            counter_new,         // this is the culprit; casting does not help either
+                            0,
+                            sizeof(t_counter),
+                            CLASS_DEFAULT,
+                            A_DEFFLOAT,          // the object takes one argument which is a floating-point and defaults to 0
+                            0);
 
-  /* call a function when object gets banged */
+  // call a function when object gets banged
   class_addbang(counter_class, counter_bang);
 }
+#pragma GCC diagnostic pop
